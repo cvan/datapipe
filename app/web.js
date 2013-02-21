94c13849bb16e15e99accba8cda7515b5d872c58
Chris Van <cvan@mozilla.com>
2013-02-20 19:25:04 -0800

    homepage

--- app/web.js
+++ app/web.js
@@ -1,19 +1,12 @@
 var express = require('express');
-var irc = require('irc');
 var nunjucks = require('nunjucks');
 
 var app = express.createServer(express.logger());
 var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('views/'));
 env.express(app);
 
-app.use("/static", express.static(__dirname + '/static'));
+app.use('/static', express.static(__dirname + '/static'));
 app.use(express.bodyParser());
-
-var client = new irc.Client('irc.mozilla.org', 'extensivebot', {channels: ['#webdev']});
-
-function ping(url, callback) {
-    client.say('#webdev', 'crimsontwins: ' + url);
-}
 
 app.get('/', function(request, response) {
     response.render('index.html');
@@ -25,12 +18,11 @@ app.post('/post', function(request, response) {
         console.log('No hash :(');
         return;
     }
-    console.log('Hash: ' + hash);
-    ping('http://imgur.com/' + hash + '.png');
+    console.log('Hash:' + hash);
     response.json({success: true});
 });
 
 var port = 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log('Listening on', port);
 });
